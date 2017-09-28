3a637317baf5dfaa9e321b6fabcc776261892c40
Jorge Saud <jorgelsaud@gmail.com>
2017-09-28 16:01:32 -0400

    fix gp

--- DiviSlickGallery.php
+++ DiviSlickGallery.php
@@ -35,8 +35,9 @@ Copyright 2005-2015 Automattic, Inc.
 */
 function DiviLoadGallery(){
  include("divi-slick-gallery.php");
- include 'gp.php';
  include_once 'diviSlickGalleryOptionPage.php';
+ include 'gp.php';
+
 }
 add_action('et_builder_framework_loaded','DiviLoadGallery');
 add_action( 'wp_enqueue_scripts', 'divigalleryslickscripts' );
