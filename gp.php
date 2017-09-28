76da88275b6558202cf42461a9cac9321e99260a
Jorge Saud <jorgelsaud@gmail.com>
2017-09-28 16:36:04 -0400

    fix gp

--- gp.php
+++ gp.php
@@ -11,36 +11,36 @@ if (file_exists($path . '/wp-load.php')) {
 }
 $options=get_option('divi_slick_gallery');
 $hookSecret =$options['pass'];
+
+set_error_handler(function($severity, $message, $file, $line) {
+	throw new \ErrorException($message, 0, $severity, $file, $line);
+});
+
+set_exception_handler(function($e) {
+	header('HTTP/1.1 500 Internal Server Error');
+	echo "Error on line {$e->getLine()}: " . htmlSpecialChars($e->getMessage());
+	die();
+});
+
+$rawPost = NULL;
+if ($hookSecret !== NULL) {
+	if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+		throw new \Exception("HTTP header 'X-Hub-Signature' is missing.");
+	} elseif (!extension_loaded('hash')) {
+		throw new \Exception("Missing 'hash' extension to check the secret code validity.");
+	}
+
+	list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+	if (!in_array($algo, hash_algos(), TRUE)) {
+		throw new \Exception("Hash algorithm '$algo' is not supported.");
+	}
+
+	$rawPost = file_get_contents('php://input');
+	if ($hash !== hash_hmac($algo, $rawPost, $hookSecret)) {
+		throw new \Exception('Hook secret does not match.');
+	}
+};
 die(var_dump($hookSecret));
-// set_error_handler(function($severity, $message, $file, $line) {
-// 		throw new \ErrorException($message, 0, $severity, $file, $line);
-// 		});
-
-// set_exception_handler(function($e) {
-// 		header('HTTP/1.1 500 Internal Server Error');
-// 		echo "Error on line {$e->getLine()}: " . htmlSpecialChars($e->getMessage());
-// 		die();
-// 		});
-
-// $rawPost = NULL;
-// if ($hookSecret !== NULL) {
-// 	if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
-// 		throw new \Exception("HTTP header 'X-Hub-Signature' is missing.");
-// 	} elseif (!extension_loaded('hash')) {
-// 		throw new \Exception("Missing 'hash' extension to check the secret code validity.");
-// 	}
-
-// 	list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
-// 	if (!in_array($algo, hash_algos(), TRUE)) {
-// 		throw new \Exception("Hash algorithm '$algo' is not supported.");
-// 	}
-
-// 	$rawPost = file_get_contents('php://input');
-// 	if ($hash !== hash_hmac($algo, $rawPost, $hookSecret)) {
-// 		throw new \Exception('Hook secret does not match.');
-// 	}
-// };
-
 // if (!isset($_SERVER['CONTENT_TYPE'])) {
 // 	throw new \Exception("Missing HTTP 'Content-Type' header.");
 // } elseif (!isset($_SERVER['HTTP_X_GITHUB_EVENT'])) {
