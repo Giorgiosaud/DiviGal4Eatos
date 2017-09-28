dcf3e4b47a52af23971fae30d65582add83b7157
Jorge Saud <jorgelsaud@gmail.com>
2017-09-28 16:26:16 -0400

    fix gp

--- gp.php
+++ gp.php
@@ -2,7 +2,7 @@
 $path = dirname(__FILE__) . '/../../..';
 //load wp
 if (file_exists($path . '/wp-load.php')) {
-	die('file exist');
+	
 	include_once $path . '/wp-load.php';
 } elseif (file_exists($path . '/../wp-load.php')) {
 	include_once $path . '/../wp-load.php';
